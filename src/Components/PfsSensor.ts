a7367ce446813966223540b4169f901053d12fec
bodabie <adamb@gmx.net>
2025-08-11 16:58:54 +0200

    feat: add PFS sensor component with block and trait configuration

--- src/Components/PfsSensor.ts
+++ src/Components/PfsSensor.ts
@@ -3,10 +3,14 @@ import { EditorSetupable } from "../Common/EditorSetupable";
 
 export class PfsSensor implements EditorSetupable {
 
-    
+
 
     editor: Editor;
     private readonly BlockCategory = 'PFS';
+
+    private readonly blockIconHtml = `<svg viewBox="0 0 24 24">
+        <path fill="currentColor" d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20Z" />
+    </svg>`;
 
     constructor(editor: Editor) {
         this.editor = editor;
@@ -18,19 +22,50 @@ export class PfsSensor implements EditorSetupable {
     */
     setup(): void {
         this.addBlock();
+        this.addTraits();
     }
 
     private addBlock(): void {
         this.editor.Blocks.add('pfs-sensor', {
             label: 'PFS Sensor',
             category: this.BlockCategory,
-            attributes: {
-                class: 'fa fa-square',
-                title: 'PFS Sensor',
-            },
+            media: this.blockIconHtml,
             content: `<section class="bdg-sect">
         <div data-pfs-config='{"plcVariable": "tempSensor", "unit": "C"}'>Sensor</div>
       </section>`,
         });
+    }
+
+    private addTraits(): void {
+        // this.editor.Components.addType('input', {
+        //     isComponent: (el) => el.tagName === 'INPUT',
+        //     model: {
+        //       defaults: {
+        //         traits: [
+        //           // Strings are automatically converted to text types
+        //           'name', // Same as: { type: 'text', name: 'name' }
+        //           'placeholder',
+        //           {
+        //             type: 'select', // Type of the trait
+        //             name: 'type', // (required) The name of the attribute/property to use on component
+        //             label: 'Type', // The label you will see in Settings
+        //             options: [
+        //               { id: 'text', label: 'Text' },
+        //               { id: 'email', label: 'Email' },
+        //               { id: 'password', label: 'Password' },
+        //               { id: 'number', label: 'Number' },
+        //             ],
+        //           },
+        //           {
+        //             type: 'checkbox',
+        //             name: 'required',
+        //           },
+        //         ],
+        //         // As by default, traits are bound to attributes, so to define
+        //         // their initial value we can use attributes
+        //         attributes: { type: 'text', required: true },
+        //       },
+        //     },
+        //   });
     }
 }
